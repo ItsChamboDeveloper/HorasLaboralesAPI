f6efc91334ce31cc94556b07f53d08832550e93d
kotsxd <20230315@ricaldone.edu.sv>
2026-09-17 09:07:45 -0600

    commit

--- src/main/java/HorasLaborales/demo/HorasLaboralesAutomotrizApplication.java
+++ src/main/java/HorasLaborales/demo/HorasLaboralesAutomotrizApplication.java
@@ -1,6 +1,5 @@
 package HorasLaborales.demo;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,9 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class HorasLaboralesAutomotrizApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(HorasLaboralesAutomotrizApplication.class, args);
 
